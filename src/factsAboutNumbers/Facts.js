30a2ec611337e546c369dc17b94273f28424bd20
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-06 21:08:03 +0200

    Add states to Facts component

--- src/factsAboutNumbers/Facts.js
+++ src/factsAboutNumbers/Facts.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import {TypeContext} from "./TypeContext";
+import {NumberContext} from "./NumberContext";
 
 export default function Facts() {
+
+    const [fact, setFact]=useState("");
+    const [isLoading, setLoading]=useState(false);
+    const [type] = useContext(TypeContext);
+    const [number] = useContext(NumberContext);
+
+
     return (
         <div>
             <h1>Fact</h1>
