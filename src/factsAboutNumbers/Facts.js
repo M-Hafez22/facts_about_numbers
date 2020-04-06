cf3209bb64da0fac7a7a318409a0888d6b77e442
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-06 21:13:17 +0200

    Fetch data

--- src/factsAboutNumbers/Facts.js
+++ src/factsAboutNumbers/Facts.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import {TypeContext} from "./TypeContext";
 import {NumberContext} from "./NumberContext";
 
@@ -8,6 +8,18 @@ export default function Facts() {
     const [isLoading, setLoading]=useState(false);
     const [type] = useContext(TypeContext);
     const [number] = useContext(NumberContext);
+
+    useEffect(() => {
+        setLoading(true)
+        console.log(isLoading)
+        fetch(`http://numbersapi.com/${number}${type === "number" ? "" : "/"+type}`)
+            .then((resp) => resp.text())
+            .then((data) => {
+                console.log(data);
+                number !== "" && setFact(data);
+                setLoading(false);
+            })
+    },[type, number])
 
 
     return (
