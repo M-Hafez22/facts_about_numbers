99f58504bfd6fb5b3df6cf616099266e1c7d9769
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-06 04:57:23 +0200

    Add app Header

--- src/factsAboutNumbers/Header.js
+++ src/factsAboutNumbers/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div>
-            <h1>Header Component</h1>
+            <h1>random facts</h1>
         </div>
     )
 }
