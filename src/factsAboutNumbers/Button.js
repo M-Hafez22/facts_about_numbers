2c52d4d13539650d54cc1382bc1eb3eeccfa72b7
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-04-06 21:30:03 +0200

    highlight the current type

--- src/factsAboutNumbers/Button.js
+++ src/factsAboutNumbers/Button.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useContext}  from 'react';
+import {TypeContext} from "./TypeContext";
 
 export default function Button({changeType, name}) {
+
+    const [type] = useContext(TypeContext);
+
     return (
         <div>
             <button 
-                className="type__buttons__btn"
+                className={`type__buttons__btn ${type === name ?  'active' : ""}`}
                 onClick={(event) => changeType(event)}
             >
                 {name}
