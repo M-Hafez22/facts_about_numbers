94ff072e9787a7e26cb8ad47f6e88ae1238b362a
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-07-13 22:36:30 +0200

    Updates

---
+++ precache-manifest.c056c7acb084d5c52abfe58b1ef134d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e8a8b47c44972b3704fcb77f1dd400",
+    "url": "/facts_about_numbers/index.html"
+  },
+  {
+    "revision": "0fd1a4645b322898d6a1",
+    "url": "/facts_about_numbers/static/css/main.1cdb322c.chunk.css"
+  },
+  {
+    "revision": "fb4b596c5236b75c9224",
+    "url": "/facts_about_numbers/static/js/2.802fa6d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facts_about_numbers/static/js/2.802fa6d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd1a4645b322898d6a1",
+    "url": "/facts_about_numbers/static/js/main.0eadc7bc.chunk.js"
+  },
+  {
+    "revision": "6381a3d5426bc1077860",
+    "url": "/facts_about_numbers/static/js/runtime-main.42e3828e.js"
+  }
+]);
